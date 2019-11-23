b3a6294541d6db99380d4259b4bbd440fa3799f6
Elbrinner <elbrinner@elbrinner.com>
2019-11-23 22:27:23 +0100

    inicio de la demo

---
+++ AccesibilidadDemo/AccesibilidadDemo/AccesibilidadDemo/Pages/HomePage.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace AccesibilidadDemo.Pages
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class HomePage : ContentPage
+    {
+        public HomePage()
+        {
+            InitializeComponent();
+        }
+
+        private async void Texto1_click(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new TextoFijoPage());
+        }
+
+        private async void Texto2_click(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new TextoSistemaPage());
+        }
+    }
+}
