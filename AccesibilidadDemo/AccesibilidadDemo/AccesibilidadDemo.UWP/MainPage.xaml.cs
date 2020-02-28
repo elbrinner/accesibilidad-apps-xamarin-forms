fdf45e41be70c60070fcdb4878dcdb6bc2d52313
Elbrinner <elbrinner@elbrinner.com>
2020-02-28 20:45:54 +0100

    Actualizando xamarin.forms y agregando soporte para UWP

--- AccesibilidadDemo/AccesibilidadDemo/AccesibilidadDemo.UWP/MainPage.xaml.cs
+++ AccesibilidadDemo/AccesibilidadDemo/AccesibilidadDemo.UWP/MainPage.xaml.cs
@@ -6,6 +6,7 @@ using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
 using Windows.UI.Xaml;
+using Windows.UI.Xaml.Automation.Peers;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
 using Windows.UI.Xaml.Data;
@@ -17,11 +18,46 @@ namespace AccesibilidadDemo.UWP
 {
     public sealed partial class MainPage
     {
+        public static bool IsNaratorActive = false;
         public MainPage()
         {
             this.InitializeComponent();
 
             LoadApplication(new AccesibilidadDemo.App());
+        }
+        private bool isAutomationPeerCreated = false;
+
+        private bool IsAutomationPeerAttached => this.isAutomationPeerCreated || AutomationPeer.ListenerExists(AutomationEvents.PropertyChanged);
+
+        //triggered everytime you run narrator or any other screen reading software that is based on accessing automation properties
+        protected override AutomationPeer OnCreateAutomationPeer()
+        {
+            if (!this.IsAutomationPeerAttached)
+            {
+                this.isAutomationPeerCreated = true;
+                this.OUR_LOGIC_BASED_ON_ATTACHED_NARRATOR();
+            }
+            return null;
+        }
+
+        protected override void OnNavigatedFrom(NavigationEventArgs e)
+        {
+            base.OnNavigatedFrom(e);
+            this.isAutomationPeerCreated = false;
+        }
+
+        protected override async void OnNavigatedTo(NavigationEventArgs e)
+        {
+            base.OnNavigatedTo(e);
+            if (IsAutomationPeerAttached)
+            {
+                this.OUR_LOGIC_BASED_ON_ATTACHED_NARRATOR();
+            }
+        }
+
+        private void OUR_LOGIC_BASED_ON_ATTACHED_NARRATOR()
+        {
+            IsNaratorActive = true;
         }
     }
 }
--- AccesibilidadDemo/AccesibilidadDemo/AccesibilidadDemo.UWP/Services/Accessibility.cs
+++ AccesibilidadDemo/AccesibilidadDemo/AccesibilidadDemo.UWP/Services/Accessibility.cs
@@ -1,35 +1,40 @@
-﻿using System;
-using System.Collections.Generic;
-using AccesibilidadDemo.Droid.Services;
+﻿
 using AccesibilidadDemo.Service;
-using Android.AccessibilityServices;
-using Android.Content;
-using Android.OS;
-using Android.Views.Accessibility;
-using Android.Widget;
-using Xamarin.Forms;
+using AccesibilidadDemo.UWP.Services;
+using Windows.UI.Xaml.Automation.Peers;
+
+
+
+using System;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Navigation;
+using Windows.Data.Xml.Dom;
+using Windows.UI.Notifications;
+using System.IO;
+using System.Runtime.InteropServices;
+using System.ComponentModel;
 
 [assembly: Xamarin.Forms.Dependency(typeof(Accessibility))]
 namespace AccesibilidadDemo.UWP.Services
 {
     public class Accessibility : IAccessibility
     {
-        Context context = Forms.Context;
-
         public bool IsVoiceOverRunning()
         {
-           
-            return AutomationPeer.ListenerExists(AutomationEvents.AutomationFocusChanged); ;
+            return MainPage.IsNaratorActive;
         }
 
         public void PostVoiceOver(string textToSpeak)
         {
             if (IsVoiceOverRunning())
             {
+
+
                 var xmlToastTemplate = "<toast launch=\"app-defined-string\">" +
                           "<visual>" +
                             "<binding template =\"ToastGeneric\">" +
-                             
+
                               "<text>" +
                                textToSpeak +
                               "</text>" +
@@ -45,10 +50,16 @@ namespace AccesibilidadDemo.UWP.Services
                 var toastNotification = new ToastNotification(xmlDocument);
                 var notification = ToastNotificationManager.CreateToastNotifier();
                 notification.Show(toastNotification);
+
+
             }
-         
+
         }
 
-        
+       
+
+
+
+
     }
 }
